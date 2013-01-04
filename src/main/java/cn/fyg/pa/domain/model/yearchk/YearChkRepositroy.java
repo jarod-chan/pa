3a7292578cc1c463ad73463f9a472f1e29f496ca
jhon_chen <jhon.chen@gmail.com>
2013-01-04 11:36:24 +0800

    去除老版本代码
    Change-Id: I867b89280620b9fad34032517b0bdeebdd29bcbe

--- src/main/java/cn/fyg/pa/domain/model/yearchk/YearChkRepositroy.java
+++ src/main/java/cn/fyg/pa/domain/model/yearchk/YearChkRepositroy.java
@@ -3,12 +3,9 @@ package cn.fyg.pa.domain.model.yearchk;
 import java.util.List;
 
 import cn.fyg.pa.domain.model.person.Person;
-import cn.fyg.pa.interfaces.module.person.yearchk.bak.PersonChkBean;
 
 public interface YearChkRepositroy {
 
-
-	List<PersonChkBean> getPersonYearChkResult(Long year, Person person);
 	
 	List<Fycheck> getPersonYearChkAboutPerson(Long year,Person aboutPerson,Person chkPerson);
 	
